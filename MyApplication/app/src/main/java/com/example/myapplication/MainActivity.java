419a4bf04392e65c7b74b2115cd8f062a17fd868
Cogi Siga <cogy@gmail.com>
2019-07-10 21:49:41 +0900

    Exchange Message Between Activity

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        EditText editText = findViewById(R.id.user_message);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, MessageActivity.class);
+        intent.putExtra("EXTRA_MESSAGE",message);
+        startActivity(intent);
+
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
+
+    }
+
+
+}
